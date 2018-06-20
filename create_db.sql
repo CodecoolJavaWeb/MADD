dc36e1caa5690d375a7f89b2771f3281f8855392
Senseinvader <d.ivantsov1983@gmail.com>
2018-06-20 14:59:05 +0200

    update apps erd and script for database creation

--- create_db.sql
+++ create_db.sql
@@ -1,20 +1,26 @@
-CREATE TABLE userr(
+CREATE TABLE IF NOT EXISTS userr(
 	id_user serial PRIMARY KEY,
+	first_name varchar(30) not null,
+	last_name varchar(30) not null,
+	login varchar(20)not null unique,
 	user_role integer
 );
 
 
 CREATE TABLE IF NOT EXISTS user_data (
-	login text,
-	password text,
+	login varchar(20),
+	password varchar(20),
 	id_user integer,
 	FOREIGN KEY (id_user) REFERENCES userr (id_user)
 	ON DELETE CASCADE ON UPDATE NO ACTION
 );
 
 
-CREATE TABLE IF NOT EXISTS class (
-	id_class serial PRIMARY KEY
+CREATE TABLE IF NOT EXISTS administrator (
+	id_admin serial PRIMARY KEY,
+	id_user integer,
+	FOREIGN KEY (id_user) REFERENCES userr (id_user)
+	ON DELETE CASCADE ON UPDATE NO ACTION
 );
 
 
@@ -24,67 +30,92 @@ CREATE TABLE IF NOT EXISTS mentor (
 	FOREIGN KEY (id_user) REFERENCES userr (id_user)
 	ON DELETE CASCADE ON UPDATE NO ACTION,
 	id_class integer,
-	FOREIGN KEY (id_class) REFERENCES class (id_class)
+	FOREIGN KEY (id_class) REFERENCES cool_class (id_class)
 	ON DELETE CASCADE ON UPDATE NO ACTION
 );
 
 
-CREATE TABLE mentor_class (
-	id_mentor integer,
-	FOREIGN KEY (id_mentor) REFERENCES mentor (id_mentor)
-    ON DELETE CASCADE ON UPDATE NO ACTION,
-	id_class integer,
-	FOREIGN KEY (id_class) REFERENCES class (id_class)
-);
-
-
-CREATE TABLE student (
+CREATE TABLE IF NOT EXISTS student (
 	id_student serial PRIMARY KEY,
 	id_user integer,
 	FOREIGN KEY (id_user) REFERENCES userr (id_user),
 	id_class integer,
-	FOREIGN KEY (id_class) REFERENCES class(id_class)
+	FOREIGN KEY (id_class) REFERENCES cool_class (id_class)
 );
 
 
-CREATE TABLE admin (
-	id_admin serial PRIMARY KEY,
-	id_user integer,
-	FOREIGN KEY (id_user) REFERENCES userr (id_user)
-	ON DELETE CASCADE ON UPDATE NO ACTION
+CREATE TABLE IF NOT EXISTS cool_class (
+	id_class serial PRIMARY KEY,
+	class_name varchar(20)
 );
 
 
-CREATE TABLE backpack (
-	id_backpack serial PRIMARY KEY,
-	id_student integer,
- 	FOREIGN KEY (id_student) REFERENCES student (id_student)
-	ON DELETE CASCADE ON UPDATE NO ACTION,
-	current_coins integer,
-	total_coins integer
+CREATE TABLE IF NOT EXISTS mentor_2_class (
+	id_mentor integer,
+	PRIMARY KEY (id_mentor, id_class),
+	FOREIGN KEY (id_mentor) REFERENCES mentor (id_mentor)
+    ON DELETE CASCADE ON UPDATE NO ACTION,
+	id_class integer,
+	FOREIGN KEY (id_class) REFERENCES cool_class (id_class)
 );
 
 
-CREATE TABLE artifact (
+
+CREATE TABLE IF NOT EXISTS artifact (
 	id_artifact serial PRIMARY KEY,
+	artifact_name varchar(50),
+	price integer,
 	category integer
-	);
+);
 
-CREATE TABLE buyers_group (
+
+CREATE TABLE IF NOT EXISTS quest (
+    id_quest serial PRIMARY KEY,
+    quest_name varchar(50),
+    price integer
+);
+
+CREATE TABLE IF NOT EXISTS buyers_group (
 	id_group serial PRIMARY KEY,
 	id_artifact integer,
 	FOREIGN KEY (id_artifact) REFERENCES artifact(id_artifact)
 	ON DELETE CASCADE ON UPDATE NO ACTION
 );
 
-CREATE TABLE student_group (
-	id_group integer,
-	foreign KEY (id_group) REFERENCES buyers_group (id_group)
+
+CREATE TABLE IF NOT EXISTS student_2_buyers_group (
+    id_student integer,
+    id_group integer,
+    money_box integer,
+    PRIMARY KEY(id_student, id_group),
+    FOREIGN KEY (id_student) REFERENCES student(id_student)
 	ON DELETE CASCADE ON UPDATE NO ACTION,
-	id_student integer,
-	FOREIGN KEY (id_student) REFERENCES student(id_student)
+	FOREIGN KEY (id_group) REFERENCES buyers_group(id_group)
 	ON DELETE CASCADE ON UPDATE NO ACTION
-	);
+);
+
+
+CREATE TABLE IF NOT EXISTS student_transaction (
+    id_transaction serial NOT NULL,
+    id_student integer,
+    id_artifact integer,
+    FOREIGN KEY (id_student) REFERENCES student(id_student)
+	ON DELETE CASCADE ON UPDATE NO ACTION,
+	FOREIGN KEY (id_artifact) REFERENCES artifact(id_artifact)
+	ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
+
+CREATE TABLE IF NOT EXISTS group_transaction (
+    id_transaction serial NOT NULL,
+    id_group integer,
+    id_artifact integer,
+    FOREIGN KEY (id_group) REFERENCES buyers_group(id_group)
+	ON DELETE CASCADE ON UPDATE NO ACTION,
+	FOREIGN KEY (id_artifact) REFERENCES artifact(id_artifact)
+	ON DELETE CASCADE ON UPDATE NO ACTION
+);
+
 
 	
 
