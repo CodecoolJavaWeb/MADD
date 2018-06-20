4e16d7c234f551532e0a69288d4c453fa2774049
Senseinvader <d.ivantsov1983@gmail.com>
2018-06-20 10:59:38 +0200

    refactor and rename script to create queststore db

--- create_db.sql
+++ create_db.sql
@@ -1,50 +1,38 @@
-CREATE TABLE user (
-	
+CREATE TABLE userr(
 	id_user serial PRIMARY KEY,
 	user_role integer
 );
 
 
-CREATE TABLE user_data (
-	
+CREATE TABLE IF NOT EXISTS user_data (
 	login text,
 	password text,
 	id_user integer,
-	FOREIGN KEY (id_user) REFERENCES user (id_user)
-	ON DELETE CASCADE ON UPDATE NO ACTION,
+	FOREIGN KEY (id_user) REFERENCES userr (id_user)
+	ON DELETE CASCADE ON UPDATE NO ACTION
 );
 
 
-CREATE TABLE user_data (
-	
-	login text,
-	password text,
-	id_user integer,
-	FOREIGN KEY (id_user) REFERENCES user (id_user)
-	ON DELETE CASCADE ON UPDATE NO ACTION,
+CREATE TABLE IF NOT EXISTS class (
+	id_class serial PRIMARY KEY
 );
 
 
-CREATE TABLE class (
-	id_class serial PRIMARY KEY;
-);
-
-
-CREATE TABLE mentor (
+CREATE TABLE IF NOT EXISTS mentor (
 	id_mentor serial PRIMARY KEY,
 	id_user integer,
-	FOREIGN KEY (id_user) REFERENCES user (id_user),
+	FOREIGN KEY (id_user) REFERENCES userr (id_user)
 	ON DELETE CASCADE ON UPDATE NO ACTION,
 	id_class integer,
 	FOREIGN KEY (id_class) REFERENCES class (id_class)
-	ON DELETE CASCADE ON UPDATE NO ACTION,
+	ON DELETE CASCADE ON UPDATE NO ACTION
 );
 
 
 CREATE TABLE mentor_class (
 	id_mentor integer,
-	FOREIGN KEY (id_mentor) REFERENCES mentor (id_mentor),
-ON DELETE CASCADE ON UPDATE NO ACTION,
+	FOREIGN KEY (id_mentor) REFERENCES mentor (id_mentor)
+    ON DELETE CASCADE ON UPDATE NO ACTION,
 	id_class integer,
 	FOREIGN KEY (id_class) REFERENCES class (id_class)
 );
@@ -53,7 +41,7 @@ ON DELETE CASCADE ON UPDATE NO ACTION,
 CREATE TABLE student (
 	id_student serial PRIMARY KEY,
 	id_user integer,
-	FOREIGN KEY (id_user) REFERENCES user (id_user),
+	FOREIGN KEY (id_user) REFERENCES userr (id_user),
 	id_class integer,
 	FOREIGN KEY (id_class) REFERENCES class(id_class)
 );
@@ -62,7 +50,7 @@ CREATE TABLE student (
 CREATE TABLE admin (
 	id_admin serial PRIMARY KEY,
 	id_user integer,
-	FOREIGN KEY (id_user) REFERENCES user (id_user)
+	FOREIGN KEY (id_user) REFERENCES userr (id_user)
 	ON DELETE CASCADE ON UPDATE NO ACTION
 );
 
@@ -70,8 +58,8 @@ CREATE TABLE admin (
 CREATE TABLE backpack (
 	id_backpack serial PRIMARY KEY,
 	id_student integer,
- 	FOREIGN KEY (id_sudent) REFERENCES student(id_student)
-	ON DELETE CASCADE ON UPDATE NO ACTION
+ 	FOREIGN KEY (id_student) REFERENCES student (id_student)
+	ON DELETE CASCADE ON UPDATE NO ACTION,
 	current_coins integer,
 	total_coins integer
 );
@@ -79,13 +67,11 @@ CREATE TABLE backpack (
 
 CREATE TABLE artifact (
 	id_artifact serial PRIMARY KEY,
-	category integer,
-	FOREIGN KEY (id_group) REFFERENCES group (id_group)
-	ON DELETE CASCADE ON UPDATE NO ACTION
-);
+	category integer
+	);
 
-CREATE TABLE group (
-	id_group PRIMARY KEY,
+CREATE TABLE buyers_group (
+	id_group serial PRIMARY KEY,
 	id_artifact integer,
 	FOREIGN KEY (id_artifact) REFERENCES artifact(id_artifact)
 	ON DELETE CASCADE ON UPDATE NO ACTION
@@ -93,11 +79,12 @@ CREATE TABLE group (
 
 CREATE TABLE student_group (
 	id_group integer,
-	FOREGIN KEY (id_group) REFFERENCES group (id_group)
-	ON DELETE CASCADE ON UPDATE NO ACTION
+	foreign KEY (id_group) REFERENCES buyers_group (id_group)
+	ON DELETE CASCADE ON UPDATE NO ACTION,
 	id_student integer,
-	FOREIGN KEY (id_student) REFFERENCES student(id_student)
+	FOREIGN KEY (id_student) REFERENCES student(id_student)
 	ON DELETE CASCADE ON UPDATE NO ACTION
+	);
 
 	
 
