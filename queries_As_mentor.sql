4852a8ff8c2a88e4c127ca0ccce249c2d1314ee2
michaal111 <michalgrzegorczyk19@gmail.com>
2018-06-21 23:24:48 +0200

    Update queries

--- queries_As_mentor.sql
+++ queries_As_mentor.sql
@@ -1,57 +1,46 @@
-AS A MENTOR
+--AS A MENTOR
 
-6.
-
-BEGIN;
+--6. (id_class = 1 podane opcjonalnie, podaje to mentor po wyświetleniu klas i sprawdzeniu id)
+BEGIN TRANSACTION;
 INSERT INTO app_user (name, last_name, phone, email, role) 
-VALUES ('Ania', 'ffidjfid', '123123132', 'mdsims@mids', 'student')
+VALUES ('Ania', 'ffidjfid', '123123132', 'dupa@mids', 'student');
 
-INSERT INTO student(id_user, id_class, money, total_money)
-VALUES ((SELECT id_user form app_user
-	WHERE email = 'mdsims@mids'), ? , 0, 0)
+INSERT INTO student(id_user, id_class, current_money, total_money)
+VALUES ((SELECT id_user FROM app_user WHERE email = 'dupa@mids'), 1, 0, 0);
 COMMIT;
 
-7,8
 
-INSERT INTO quest (name, price, category, description)
-VALUES ('si', 20, 'basic', 'si finished blabla')
+--7,8
+INSERT INTO quest (quest_name, price, category, description)
+VALUES ('si', 20, 'basic', 'si finished blabla');
 
-9.
+--9.
+INSERT INTO artifact(artifact_name, category, description, price)
+VALUES ('nazwaArtefaktuHEHE', 'magic', '60 min workshop', 1000);
 
-INSERT INTO artifact(category, description, price)
-VALUES ('magic', ' 60 min workshop', 1000)
 
-
-10.
-
+--10. (podajemy price przez input, opcjonalnie 40
+--id_quest wybiera mentor po uprzednim wyświetleniu i wybraniu, opcjonalnie 1
 SELECT * FROM quest
 
 UPDATE quest
-SET price = ???
-WHERE
- id_quest = ?;
+SET price = 40
+WHERE id_quest = 1;
 
 
-11. 
-
-INSERT INTO artifact (category, price, description)
-VALUES ('magic', 40, 'eefsfdsgis')
-
-12.
-
-SELECT * FROM quest
-
-SELECT price * FROM quest 
-WHERE id_quest = ???
+--11. 
+INSERT INTO artifact (artifact_name, category, price, description)
+VALUES ('artifactName', 'magic', 40, 'eefsfdsgis');
 
 
+--12. (ktoś tam podaje id_quest, nie pamiętam kto)
 UPDATE student
-SET money = money + price
+SET money = money + (SELECT price FROM quest WHERE id_quest = 1);
 
 
-13.
+--13. TRZEBA SPRAWZDZIĆ QUANTITY JAK WIEKSZE NIŻ 1 TO ODEJMIJ JEDNO JEŚLI =1 TO DELETE:)
 
-SELECT * FROM students
+SELECT * FROM student
 
 SELECT * FROM student_artifact
 WHERE id_student = ?
@@ -69,6 +58,12 @@ WHERE id_student = ?
 SELECT * FROM student_transaction
 WHERE id_stundet ??
 
+
+------ PRZYPISYWANIE NOWEGO ITEMU DO STUDENTA (jeśli ma to insert jeśli nie to update quantity +1)
+UPDATE student_artifact SET quantity = quantity + 1 WHERE id_student = 1;
+INSERT INTO student_artifact (id_student, id_artifact, quantity)
+       SELECT 1, 2, 1
+       WHERE NOT EXISTS (SELECT 1 FROM student_artifact WHERE id_student = 1);
 
 
 
