35f29b7042802985780cd85307df8e13226fdcbf
idzia <ania.idzi@gmail.com>
2018-07-04 10:38:52 +0200

    Add group shopping and one student shopping

--- resources/front_end/javascript/store.js
+++ resources/front_end/javascript/store.js
@@ -1,9 +1,8 @@
 function calculetePrice() {
     var quantityStudents = document.getElementById('qS').value;
-    var price = 5;
+    var price = 4;
     var buyButton = document.getElementById('buy');
 
-    console.log(quantityStudents);
     if (price % quantityStudents != 0 ) {
         buyButton.setAttribute("disabled", "true")
         return price;
@@ -14,4 +13,21 @@ function calculetePrice() {
 
 function refresh() {
     document.getElementById('topay').innerHTML = calculetePrice();
+}
+
+function calculetePrice2() {
+    var quantityStudents = document.getElementById('qS2').value;
+    var price = 1;
+    var buyButton = document.getElementById('buy2');
+
+    if (price % quantityStudents != 0 ) {
+        buyButton.setAttribute("disabled", "true")
+        return price;
+    } else
+        buyButton.removeAttribute("disabled")
+        return (price / quantityStudents);
+}
+
+function refresh2() {
+    document.getElementById('topay2').innerHTML = calculetePrice2();
 }
