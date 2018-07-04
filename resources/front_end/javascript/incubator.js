918d39563701fde38f38c8e05c929d216e2c96ec
Polikkox <damian.targosz11@gmail.com>
2018-07-04 11:18:26 +0200

    Add js to incubator, add inform field to store

---
+++ resources/front_end/javascript/incubator.js
@@ -0,0 +1,7 @@
+function send_form(){
+    document.getElementsByClassName("alert_button")[0].innerHTML = "*Done";
+
+    var artifact_name = document.getElementsByClassName("artifact-name")[0].value;
+    var price = document.getElementsByClassName("to-price")[0].value;
+
+}
--- resources/front_end/javascript/store.js
+++ resources/front_end/javascript/store.js
@@ -2,9 +2,11 @@ function calculetePrice() {
     var quantityStudents = document.getElementById('qS').value;
     var price = 4;
     var buyButton = document.getElementById('buy');
+    document.getElementsByClassName("alert_button")[0].innerHTML = "";
 
     if (price % quantityStudents != 0 ) {
         buyButton.setAttribute("disabled", "true")
+        document.getElementsByClassName("alert_button")[0].innerHTML = "*Not possible";
         return price;
     } else
         buyButton.removeAttribute("disabled")
