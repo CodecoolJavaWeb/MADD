9b1da45e3f4101182c685894a13c4803c13fb150
Polikkox <damian.targosz11@gmail.com>
2018-07-06 23:46:45 +0200

    Improvement to display message after join to group

--- resources/front_end/javascript/incubator.js
+++ resources/front_end/javascript/incubator.js
@@ -1,8 +1,22 @@
-function send_form(){
-    document.getElementsByClassName("alert_button")[0].innerHTML = "*Done";
+function addModal() {
 
-    var artifact_name = document.getElementsByClassName("artifact-name")[0].value;
-    var price = document.getElementsByClassName("to-price")[0].value;
-    location.reload();
-
+    const modal = document.getElementById("modalWindow");
+    modal.classList.add("show-modal");
 }
+
+function removeModal() {
+
+    const modal = document.getElementById("modalWindow");
+    modal.classList.remove("show-modal");
+}
+
+
+//function send_form(){
+//
+//    document.getElementsByClassName("alert_button")[0].innerHTML = "*Done";
+//
+//    var artifact_name = document.getElementsByClassName("artifact-name")[0].value;
+//    var price = document.getElementsByClassName("to-price")[0].value;
+//
+//
+//}
