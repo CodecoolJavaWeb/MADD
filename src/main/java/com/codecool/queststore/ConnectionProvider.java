6dbe1e5aa84b3755d43a9b6a7cc854b8931aec2e
michaal111 <michalgrzegorczyk19@gmail.com>
2018-07-17 09:54:05 +0200

    Create skeleton

--- src/main/java/com/codecool/queststore/ConnectionProvider.java
+++ src/main/java/com/codecool/queststore/ConnectionProvider.java
@@ -1,51 +1,26 @@
 package com.codecool.queststore;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
 
-public final class ConnectionProvider {
-    private static final String DB_PROPERTIES_FILE;
-    private static final Properties DB_CONNECTION_PROPERTIES;
 
-    private static String DB_URL;
-    private static String DB_USERNAME;
-    private static String DB_PASSWORD;
+public class ConnectionProvider {
+    private Connection connection;
 
-    private static Connection connection;
 
-    static {
-        DB_PROPERTIES_FILE = "database.properties";
-        DB_CONNECTION_PROPERTIES = new Properties();
-
+    public ConnectionProvider(String name, String owner, String password) {
         try {
-            Path propertiesFilePath = Paths.get("src/main/resources", DB_PROPERTIES_FILE);
-            DB_CONNECTION_PROPERTIES.load(Files.newInputStream(propertiesFilePath));
-
-            DB_URL = DB_CONNECTION_PROPERTIES.getProperty("javabase.jdbc.url");
-            DB_USERNAME = DB_CONNECTION_PROPERTIES.getProperty("javabase.jdbc.username");
-            DB_PASSWORD = DB_CONNECTION_PROPERTIES.getProperty("javabase.jdbc.password");
-        } catch (IOException e) {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + name, owner, password);
+        } catch (Exception e) {
             e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
         }
     }
 
-    public static Connection getConnection() throws PersistenceLayerException {
-        if (connection == null) {
-            try {
-                connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-            } catch (SQLException e) {
-                throw new PersistenceLayerException("Can't get connection to the database");
-            }
-        }
+
+    public Connection getConnection() {
         return connection;
-    }
-
-    private static class PersistenceLayerException extends Exception {
     }
 }
